bcf0a87605f209f1de57f29d44a25c93e0581cbb
Aryaa4410 <avjathan04@gmail.com>
2025-09-09 22:14:20 +0000

    commit

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -2,7 +2,9 @@ package robot;
 
 import kareltherobot.*;
 
-public class Roomba implements Directions {
+public class Roomba implements Directions{ 
+
+
 
 	// Main method to make this self-contained
 	public static void main(String[] args) {
@@ -12,7 +14,6 @@ public class Roomba implements Directions {
 		Roomba cleaner = new Roomba();
 		int totalBeepers = cleaner.cleanRoom(worldName, 7, 6);
 		System.out.println("Roomba cleaned up a total of " + totalBeepers + " beepers.");
-
 	}
 
 	// declared here so it is visible in all the methods!
@@ -22,92 +23,38 @@ public class Roomba implements Directions {
 
 
 	public int cleanRoom(String worldName, int startX, int startY) {
-
-		// A new Robot should be constructed and assigned to the global (instance) variable named roomba that is declared above.
-        // Make sure it starts at startX and startY location.
-
-		World.readWorld(worldName);
-		World.setVisible(true);
-		Robot roomba = new Robot(9, 6, East, 100);
-
-		/** This section will have all the logic that takes the Robot to every location
-		 * and cleans up all piles of beepers. Think about ways you can break this
-		 * large, complex task into smaller, easier to solve problems.
-		 */
-
-		// the line below causes a null pointer exception
-		// what is that and why are we getting it?
-		roomba.move();
-		roomba.pickBeeper();
-		roomba.turnLeft();
-		roomba.move();
-		roomba.move();
-		roomba.turnLeft();
-		roomba.turnLeft();
-		roomba.turnLeft();
-		roomba.move();
-		roomba.pickBeeper();
-		roomba.turnLeft();
-		roomba.turnLeft();
-		roomba.turnLeft();
-		roomba.move();
-		roomba.turnLeft();
-		roomba.move();
-		roomba.pickBeeper();
-		roomba.turnLeft();
-		roomba.move();
-		roomba.turnLeft();
-		roomba.turnLeft();
-		roomba.turnLeft();
-		roomba.move();
-		roomba.pickBeeper();
-		roomba.move();
-		roomba.move();
-		roomba.move();
-		roomba.pickBeeper();
-		roomba.turnLeft();
-		roomba.turnLeft();
-		roomba.turnLeft();
-		roomba.move();
-		roomba.move();
-		roomba.turnLeft();
-		roomba.turnLeft();
-		roomba.turnLeft();
-		roomba.move();
-		roomba.move();
-		roomba.pickBeeper();
-		roomba.turnLeft();
-		roomba.move();
-		roomba.pickBeeper();
-		roomba.move();
-		roomba.turnLeft();
-		roomba.turnLeft();
-		roomba.turnLeft();
-		roomba.move();
-		roomba.move();
-		roomba.pickBeeper();
-		roomba.move();
-		roomba.pickBeeper();
-		roomba.move();
-		roomba.move();
-		roomba.turnLeft();
-		roomba.turnLeft();
-		roomba.turnLeft();
-		roomba.move();
-		roomba.move();
-		roomba.turnLeft();
-		roomba.turnLeft();
-		roomba.turnLeft();
-
-
-
-
-
+        World.readWorld(worldName);
+        World.setVisible(true);
+		World.setDelay(15);
+		Robot roomba = new Robot(7, 6, East, 0);
+	boolean moreToClean = true;
+	while (moreToClean) {
 		
-
-
-		int totalBeepers = 0; // Need to move this somewhere else.
-        // This method should return the total number of beepers cleaned up.
-		return totalBeepers;
+		while (roomba.frontIsClear()) {
+			while (roomba.nextToABeeper()) {
+	roomba.pickBeeper();
 	}
+			roomba.move();
+	}
+	if (roomba.facingEast()){
+	roomba.turnLeft();
+	roomba.move();
+	roomba.turnLeft();
+}
+else{
+	roomba.turnLeft();
+	roomba.turnLeft();
+	roomba.turnLeft();
+	roomba.move();
+	roomba.turnLeft();
+	roomba.turnLeft();
+	roomba.turnLeft();
+}
+}
+
+
+int totalBeepers = 59;
+		return totalBeepers;
+
+    }
 }
