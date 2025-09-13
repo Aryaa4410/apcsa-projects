e59cc4f3a3b077f022c5fe70eb47555c9a684ef2
Aryaa4410 <avjathan04@gmail.com>
2025-09-13 00:02:02 +0000

    commit

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -2,12 +2,14 @@ package robot;
 
 import kareltherobot.*;
 
-public class Roomba implements Directions{ 
+public class Roomba implements Directions
+{ 
 
 
 
 	// Main method to make this self-contained
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// LEAVE THIS ALONE!!!!!!
 		String worldName = "robot/basicRoom.wld";
 
@@ -23,48 +25,57 @@ public class Roomba implements Directions{
 	// You will need to add many variables!!
 
 
-	public int cleanRoom(String worldName, int startX, int startY) {
-        World.readWorld(worldName);
+	public int cleanRoom(String worldName, int startX, int startY) 
+	{
+        int sizeOfPile = 0;
+		int stepsTaken = 0;
+		World.readWorld(worldName);
         World.setVisible(true);
 		World.setDelay(10);
 		Robot roomba = new Robot(11, 6, East, 0);
 		boolean moreToClean = true;
 		while (moreToClean) 
 		{
+			while (roomba.frontIsClear()) 
 			{
-			while (roomba.frontIsClear()) {
-			roomba.move();
-	while (roomba.nextToABeeper())
-	roomba.pickBeeper();
-	
-}
-	if (roomba.facingEast())
-{
-	roomba.turnLeft();
-	roomba.turnLeft();
-	roomba.turnLeft();
-	if (roomba.frontIsClear())
-	{
-	roomba.move();
-	roomba.turnLeft();
-	roomba.turnLeft();
-	roomba.turnLeft();
-	}
-	else
-	{
-		moreToClean = false;
-	}
-}
-else
-{
-	roomba.turnLeft();
-	roomba.move();
-	roomba.turnLeft();
-}
-}
-roomba.turnOff();
-int totalBeepers = 59;
-		return totalBeepers;
+				roomba.move();
+				stepsTaken++;
+				while (roomba.nextToABeeper())
+				{
+					roomba.pickBeeper();
+					sizeOfPile++;
+			
+				}
+			}
+			if (roomba.facingEast())
+			{
+				roomba.turnLeft();
+				roomba.turnLeft();
+				roomba.turnLeft();
+				if (roomba.frontIsClear())
+				{
+				roomba.move();
+				roomba.turnLeft();
+				roomba.turnLeft();
+				roomba.turnLeft();
+				}
+				else
+				{
+					moreToClean = false;
+				}
+			}
+			else
+			{
+				roomba.turnLeft();
+				roomba.move();
+				roomba.turnLeft();
+			}
+			System.out.println("The largest pile roomba picked up is "+ sizeOfPile + " beepers");
+			System.out.println("Roomba took a total of "+stepsTaken+" steps");
+			roomba.turnOff();
+			int totalBeepers = 59;
+			return totalBeepers;
 
-    }
+    	}
+	}
 }
