02dec308e96529b8d65d7bed8b6a4abc18c1cec3
Aryaa4410 <avjathan04@gmail.com>
2025-11-20 22:16:35 +0000

    finished part of deck.java

--- cards/Deck.java
+++ cards/Deck.java
@@ -1,7 +1,16 @@
 package cards;
+import java.util.ArrayList;
+
 
 public class Deck 
 {
-private int
+   private Card[] cards;
+   private int next = 0;
+  
+   public Deck()
+   {
+       cards = new Card[52];
+       next = 0;
 
+   }
 }
--- cards/Hand.java
+++ cards/Hand.java
@@ -2,35 +2,5 @@ package cards;
 
 public class Hand 
 {
-    private int maxCards;
-
-    public Hand(int maxCards)
-    {
-        this.maxCards = maxCards;
-    }
-
-    public int add()
-    {
-
-    }
-
-    public int length()
-    {
-        return length;
-    }
-
-    public int get()
-    {
-
-    }
-
-    public int remove()
-    {
-
-    }
-
-    public String toString()
-    {
-        
-    }
+    
 }
