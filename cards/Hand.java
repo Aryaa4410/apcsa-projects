3a3bffc6a30f61ea142495de92c4c38f550b7fb0
Aryaa4410 <avjathan04@gmail.com>
2025-11-20 22:23:26 +0000

    finished part of hand.java

--- cards/Hand.java
+++ cards/Hand.java
@@ -2,5 +2,27 @@ package cards;
 
 public class Hand 
 {
-    
+   private Card[] cards;
+   private int count;
+
+
+   public Hand(int maxNum)
+   {
+       cards = new Card[maxNum];
+       count = 0;
+   }
+    public void add(Card c){
+       if (count < cards.length){
+           cards[count] = c;
+           count++;
+       }
+   }
+
+
+   public int length()
+   {
+       return count;
+   }
+
+ 
 }
