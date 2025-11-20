f785d880487a9c145bbb7722a033d1a57547ff93
Aryaa4410 <avjathan04@gmail.com>
2025-11-20 22:25:10 +0000

    finished all of hand.java

--- cards/Hand.java
+++ cards/Hand.java
@@ -23,6 +23,42 @@ public class Hand
    {
        return count;
    }
+   public Card get(int index){
+       if (index < 0 || index >= count){
+           return null;
+       }
+       return cards[index];
+   }
+
+
+   public Card remove(int index)
+   {
+       if (index < 0 || index >= count)
+        {
+           return null;
+        }
+       Card removed = cards[index];
+
+
+       for(int i = index; i < count -1; i++)
+        {
+           cards[i] = cards[i+1];
+        }
+       cards[count - 1] = null;
+       count--;
+       return removed;
+   }
+   public String toString()
+   {
+       String result = "";
+       for (int i = 0; i<count; i++)
+        {
+           result += cards[i].toString() + " ";
+        }
+       return result.trim();
+   }
+
+
 
  
 }
