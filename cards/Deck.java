5cdeba3f9030845997424ac7cb0f69b3d9674768
Aryaa4410 <avjathan04@gmail.com>
2025-11-20 22:17:57 +0000

    finished more of deck.java

--- cards/Deck.java
+++ cards/Deck.java
@@ -11,6 +11,25 @@ public class Deck
    {
        cards = new Card[52];
        next = 0;
-
+       
+       int index = 0;
+       for (int suit = 0; suit<4; suit++){
+           for (int value = 0; value<13; value++){
+               cards[index] = new Card(suit,value);
+               index++;
+           }
+       }
    }
+
+
+   public void shuffle(){
+       for (int i = cards.length - 1; i>0; i--){
+           int n = (int)(Math.random() * (i+1));
+           Card temp = cards[i];
+           cards[i] = cards[n];
+           cards[n] = temp;
+       }
+       next = 0;
+   }
+
 }
