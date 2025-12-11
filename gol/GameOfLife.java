ee0e290b5edaad0f6db5ac517d226e27d03b5cb3
Aryaa4410 <avjathan04@gmail.com>
2025-12-11 22:36:57 +0000

    finished game of life constructor

--- gol/GameOfLife.java
+++ gol/GameOfLife.java
@@ -5,25 +5,22 @@ import java.util.Arrays;
 public class GameOfLife implements Board {
 
     // Integers: 0 or 1 for alive or dead
-    private int[][] board;
+    private int [][] board;
 
     public GameOfLife(int x, int y)
     {
-        // Construct a 2d array of the given x and y size.
+       board = new int[x][y];
     }
-
-    // Set values on the board
+     
     public void set(int x, int y, int[][] data) {
         for (int i = 0; i < data.length; i++) {
             for (int j = 0; j < data[0].length; j++) {
-                board[i + x][j + y] = data[i][j];
+                board[i + x][ j + y] = data[i][j];
             }
         }
     }
-
     // Run the simulation for a number of turns
     public void run(int turns) {
-        // call step the number of times requested
     }
 
     // Step the simulation forward one turn.
