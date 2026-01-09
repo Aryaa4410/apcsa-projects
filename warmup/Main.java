fe806303d9e2019859897cb4394c6482268b0a37
Aryaa4410 <avjathan04@gmail.com>
2026-01-09 03:05:25 +0000

    warmup code finished

--- warmup/Main.java
+++ warmup/Main.java
@@ -1,9 +1,18 @@
 package warmup;
 
-public class Main {
-
-    public static void main(String[] args) {
-        // Your code here.
-
+public class Main 
+{
+    int totalSum;
+    public static void main(String[] args) 
+    {
+        int totalSum = 0;
+        for (int i = 0; i <= 1000; i++)
+        {
+            if(i % 5 == 0 || i % 3 == 0)
+            {
+                totalSum+=i;
+            }
+        }
+        System.out.print("The total sum is " +totalSum);
     }
 }
