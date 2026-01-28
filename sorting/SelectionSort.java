d3c2fb956f93d846ba64ab08291e9364c6c0e6d0
Aryaa4410 <avjathan04@gmail.com>
2026-01-28 01:52:33 +0000

    selection sorting

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -2,8 +2,29 @@ package sorting;
 
 public class SelectionSort implements Sorter {
 
-    public void sort(int[] input) {
-        System.out.println("Selection Sort!!!");
-        //this is my project
+    public void sort(int[] input) 
+    {
+        int length = input.length();
+        for(int i=0; i< length; i++)
+        {
+            int min = input[i];
+            int indexOfMin = i;
+            for (int j = i+1; j<length-1; j++)
+            {
+                if(input[j]<min)
+                {
+                    min = input [j];
+                    indexOfMin = j;
+                }
+            }
+        }
+        swap(input, i, indexOfMin);   
     }
+    public static void swap(int[]input, int a, int b)
+    {
+        int temp = input[a];
+        input[a] = input[b];
+        input[b] = temp;    
+    }
+    System.out.println("Selection Sort!!!");
 }
