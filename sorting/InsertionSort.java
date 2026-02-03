8e98e6687450857ce62d67fcd8a55c343a6dc1c3
Aryaa4410 <avjathan04@gmail.com>
2026-02-03 22:57:43 +0000

    final commit with comments

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -7,12 +7,12 @@ public class InsertionSort implements Sorter
     {
        System.out.println("Insertion Sort!!!");
        
-       for(int i=1; i< input.length; i++)
+       for(int i=1; i< input.length; i++)//loops through every number in the array
        {
         int currentValue = input [i];
        
         int j = i-1;
-        while(j>=0 && input [j] > currentValue)
+        while(j>=0 && input [j] > currentValue)//this loops to make sure the smallest number is int the front and stops when the numbers are in order
         {
             input[j+1] = input[j];
             j--;
--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -6,25 +6,25 @@ public class SelectionSort implements Sorter {
     {
         System.out.println("Selection Sort!!!");
         int length = input.length;
-        for(int i=0; i< length-1; i++)
+        for(int i=0; i< length-1; i++)//this loops through every number in the array
         {
             int min = input[i];
             int indexOfMin = i;
-            for (int j = i+1; j<length; j++)
+            for (int j = i+1; j<length; j++)//this finds the smallest value of the array
             {
-                if(input[j]<min)
+                if(input[j]<min)//makes sure that min is always the smallest number of the array
                 {
                     min = input [j];
                     indexOfMin = j;
                 }
             }
-            swap(input, i, indexOfMin);
+            swap(input, i, indexOfMin);//swap method to make sure the smallest number is at the front
         }
     }
-    public static void swap(int[]input, int a, int b)
+    public static void swap(int[]input, int a, int b)//swaps the places of the unsorted part of the array with the smallest number in the array
     {
         int temp = input[a];
         input[a] = input[b];
-        input[b] = temp;    
+        input[b] = temp;   
     }
 }
