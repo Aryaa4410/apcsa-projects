bad5cd2d2d7959af7a95fe9aafd0196f4f4242ca
Aryaa4410 <avjathan04@gmail.com>
2026-02-04 02:21:25 +0000

    project done

--- sorting/Main.java
+++ sorting/Main.java
@@ -6,22 +6,18 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args)
     {
-        int[] testInput = randomArray(5);
-        Random random = new Random();
-        for(int i =0; i< testInput.length; i++)
-        {
-            testInput[i]= random.nextInt(5);
-        }
-        // Increase the loop count to get better results once it works.
-        // TestSuite.run(testInput, 10000);
-        TestSuite.run(testInput, 1);
+       int[]testInput = randomArray(10);
+        TestSuite.run(testInput, 100);
     }
 
     public static int[] randomArray(int length)
     {
-        // DO: make this return an actual random array of the provided length.
-        int[] a = {10, 5, 3, 2};
-        return a;
+        int[] array = new int[length];
+        for(int b = 0; b < length; b++)
+        {
+            array[b] = (int) (Math.random() *9) + 1;
+        }
+        return array;
     }
 
 }
